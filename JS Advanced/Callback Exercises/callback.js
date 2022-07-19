1c4c5775f58ff36e5c2dc609241580c21ef0bf96
phengzkie <alfie.osayan@gmail.com>
2022-07-19 19:32:26 +0800

    uplaod js advanced

---
+++ JS Advanced/Callback Exercises/callback.js
@@ -0,0 +1,38 @@
+// 1.
+function filterArray(array, callback) {
+	let newArr = [];
+	for (var i = 0; i < array.length; i++) {
+		if (callback(array[i]) === true) {
+			newArr.push(array[i]);
+		}
+	}
+	return newArr;
+}
+
+let fa = filterArray([1, 2, 3, 4, 15], function (num) {
+	return num > 10;
+});
+console.log(fa);
+
+// 2.
+function add(a, b) {
+	return function () {
+		return a + b;
+	};
+}
+let result = add(3, 5);
+console.log(result());
+
+// 3.
+function twoFunc(func1, func2) {
+	Math.random() < 0.5 ? func1() : func2();
+}
+
+twoFunc(
+	function () {
+		console.log("1");
+	},
+	function () {
+		console.log("2");
+	}
+);
